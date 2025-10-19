ddaba0cfb5561512fd5def04a6a87a6acab7f670
Mohamed Maher <m7mdellham77@gmail.com>
2025-10-19 15:03:07 +0300

    ..

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -62,15 +62,16 @@
 
         <!-- Right: Courses & Sign Up -->
         <div class="flex items-center gap-4">
-            {{-- <a href="{{ route('web.courses') }}"
-                class="flex items-center gap-2 text-xl text-gray-700 hover:text-[#176b98]">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 6v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5s3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18s-3.332.477-4.5 1.253" />
-                </svg>
-                COURSES
-            </a> --}}
+
+            @if (Auth::check())
+                <!-- زر السلة -->
+                <a href="{{ route('cart') }}"
+                    class="flex items-center gap-2 px-4 py-2 bg-[#176b98] text-white rounded-lg hover:bg-[#125a7d] transition">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span>My Cart</span>
+                </a>
+            @endif
+
 
             <div x-data="{ open: false }" class="relative">
                 <button @click="open = !open"
