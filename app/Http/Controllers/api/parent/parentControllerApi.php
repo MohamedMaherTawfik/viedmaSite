974b1dd0db82cf17d1197e96a3201851fafda0f5
Mohamed Maher <m7mdellham77@gmail.com>
2025-11-15 17:28:03 +0200

    ..

--- app/Http/Controllers/api/parent/parentControllerApi.php
+++ app/Http/Controllers/api/parent/parentControllerApi.php
@@ -49,7 +49,6 @@ class parentControllerApi extends Controller
     {
         $user = User::find(request('id'));
         $student = student::where('user_id', $user->id)->pluck('me_id')->toArray();
-        dd($student);
         $enrollments = Enrollments::where('user_id', $student)->pluck('courses_id')->toArray();
         $courses = Courses::whereIn('id', $enrollments)->get();
         return $this->success($courses, 'all courses for childrens');
