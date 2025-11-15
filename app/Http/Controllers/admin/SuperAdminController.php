0e6ef9723c2dcc806b5fa33beeba64e840f15499
Mohamed Maher <m7mdellham77@gmail.com>
2025-11-15 02:45:54 +0200

    ..

--- app/Http/Controllers/admin/SuperAdminController.php
+++ app/Http/Controllers/admin/SuperAdminController.php
@@ -126,8 +126,8 @@ class SuperAdminController extends Controller
      */
     public function schoolTeachers()
     {
-        $teachers = User::where('role', 'teacher')->where('school_id', Auth::user()->school_id)->get();
-        $applies = applyTeacher::get();
+        $teachers = User::where('role', 'teacher')->where('school_id', Auth::user()->school_id)->pluck('id');
+        $applies = applyTeacher::where('user_id', $teachers)->get();
         return view('schoolDashboard.teachers.index', compact('teachers', 'applies'));
     }
 
