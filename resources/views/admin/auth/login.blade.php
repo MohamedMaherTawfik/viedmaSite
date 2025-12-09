3811eb30f277f44dcf19f2f72dd2e3ba6f5090c2
MohamedMaherTawfik <m7mdellham77@gmail.com>
2025-12-09 19:50:27 +0200

    ..

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -18,7 +18,7 @@
         <h2 class="text-xl font-bold mb-4 text-center">Login</h2>
         <div class="mb-4">
             <label for="email" class="block text-sm font-medium text-gray-700 mb-1">البريد الإلكتروني</label>
-            <input type="email" id="email" name="email" placeholder="example@mail.com"
+            <input type="email" id="email" name="email" placeholder="email"
                 class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-500">
         </div>
         <div class="mb-4">
